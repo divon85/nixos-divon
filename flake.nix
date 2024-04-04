64dcdbc6ed919fb5192433b9f7270df7a3fa93c7
divon85 <94886650+divon85@users.noreply.github.com>
2024-04-05 01:09:28 +0900

    fix bugs and add installation scripts

--- flake.nix
+++ flake.nix
@@ -21,9 +21,9 @@
 
 # ----- USER SETTINGS ----- #
     userSettings = rec {
-        username = "igor"; # username
-        name = "Igor Novid"; # name/identifier
-        # email = "emmet@librephoenix.com"; # email (used for certain configurations)
+        username = "divon"; # username
+        name = "Divon"; # name/identifier
+        email = "igornovid@outlook.com"; # email (used for certain configurations)
         dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
         theme = "uwunicorn-yt"; # selcted theme from my themes directory (./themes/)
         wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
