f6a45ece84a7d5aa9dccd56f707fc7effd048660
divon85 <94886650+divon85@users.noreply.github.com>
2024-04-18 19:12:40 +0900

    Update configuration.nix

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -8,25 +8,25 @@
   [ 
     ../system/hardware-configuration.nix
     # ( import ../system/app/docker.nix {storageDriver = null; inherit pkgs userSettings lib;} )
-    ../system/app/gamemode.nix
-    ../system/app/prismlauncher.nix
-    ../system/app/steam.nix
-    ../system/app/virtualization.nix
-    ../system/hardware/bluetooth.nix
-    ../system/hardware/kernel.nix # Kernel config
-    ../system/hardware/opengl.nix
-    ../system/hardware/power.nix # Power management
-    ../system/hardware/printing.nix
-    ../system/hardware/systemd.nix # systemd config
-    ../system/hardware/time.nix # Network time sync
-    ../system/security/automount.nix
-    ../system/security/blocklist.nix
-    ../system/security/doas.nix
-    ../system/security/firejail.nix
-    ../system/security/firewall.nix
-    ../system/security/gpg.nix
-    ../system/security/openvpn.nix
-    ../system/style/stylix.nix
-    ../system/wm/hyprland.nix
+    # ../system/app/gamemode.nix
+    # ../system/app/prismlauncher.nix
+    # ../system/app/steam.nix
+    # ../system/app/virtualization.nix
+    # ../system/hardware/bluetooth.nix
+    # ../system/hardware/kernel.nix # Kernel config
+    # ../system/hardware/opengl.nix
+    # ../system/hardware/power.nix # Power management
+    # ../system/hardware/printing.nix
+    # ../system/hardware/systemd.nix # systemd config
+    # ../system/hardware/time.nix # Network time sync
+    # ../system/security/automount.nix
+    # ../system/security/blocklist.nix
+    # ../system/security/doas.nix
+    # ../system/security/firejail.nix
+    # ../system/security/firewall.nix
+    # ../system/security/gpg.nix
+    # ../system/security/openvpn.nix
+    # ../system/style/stylix.nix
+    # ../system/wm/hyprland.nix
   ];
 }
