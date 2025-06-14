90e759078bb21586ac8e0b495ef227feccdca362
divon85 <igornovid@outlook.com>
2025-06-14 18:57:45 +0900

    edit bootloader.nix

--- system/settings/bootloader.nix
+++ system/settings/bootloader.nix
@@ -1,6 +1,5 @@
 {
     # Bootloader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sdb";
-    boot.loader.grub.useOSProber = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 }
