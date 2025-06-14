2664f9a7d7c7c4354aa5f3870cb419ba55ee81db
divon85 <igornovid@outlook.com>
2025-06-14 17:34:55 +0900

    add modules to bunlde.nix

--- system/bundle.nix
+++ system/bundle.nix
@@ -1,5 +1,11 @@
 {
     imports = [
         ./hardware-configuration.nix
+        ./settings/bootloader.nix
+        ./settings/hyprland.nix
+        ./settings/networking.nix
+        ./settings/software.nix
+        ./settings/thunar.nix
+        ./settings/timezone.nix
     ];
 }
