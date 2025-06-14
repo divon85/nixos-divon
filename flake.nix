3cce9880832e5e995c28d69cede2056d1cdf57c5
divon85 <igornovid@outlook.com>
2025-06-14 15:19:01 +0900

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+    description = "Divon NixOS Flakes";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
+    };
+
+    outputs = inputs@{ self, ... }:
+    let
+    # ---- SYSTEM SETTINGS ---- #
+    systemSettings = {
+        architecture = "x86_64-linux"; #system architecture
+        hostname = "divon-nixos";
+        timezone = "Asia/Tokyo";
+        locale = "en_US.UTF-8";
+        bootMode = "uefi"; # bios or uefi
+        bootMountPath = "/BOOT";
+        grubDevice = "/dev/sda"; # device identifier for grub; only used for legacy (bios) boot mode
+    };
+
+    # ---- USER SETTINGS ---- #
+    userSettings = {
+        username = "igor";
+        name = "Igor Novid";
+        email = "igornovid@outlook.com";
+        dotfilesDir = "~/.dotfiles";
+    };
+
+    lib = inputs.nixpkgs.lib;
+
+    pkgs = import inputs.nixpkgs {
+        system = systemSettings.architecture;
+        config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+        };
+    };
+
+    in {
+        nixosConfigurations = {
+            system = lib.nixosSystem {
+                system = systemSettings.architecture;
+                modules = [ ./system_configurations/configuration-orig.nix ];
+                specialArgs = {
+                    inherit systemSettings;
+                    inherit userSettings;
+                    inherit inputs;
+                };
+            };
+        };
+    };
+}
