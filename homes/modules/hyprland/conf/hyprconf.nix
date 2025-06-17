c4b7a1b698f3af22db019ef5c90dfe3cb30fb555
divon85 <igornovid@outlook.com>
2025-06-17 21:43:28 +0900

    update conf

--- homes/modules/hyprland/conf/hyprconf.nix
+++ homes/modules/hyprland/conf/hyprconf.nix
@@ -123,9 +123,9 @@
 
             # See https://wiki.hyprland.org/Configuring/Variables/ for more
             general {
-                gaps_in = 5
-                gaps_out = 20
-                border_size = 2
+                gaps_in = 2
+                gaps_out = 5
+                border_size = 1
                 col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
                 col.inactive_border = rgba(595959aa)
                 allow_tearing = false
