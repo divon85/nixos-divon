389c712624d585a7d3869eaca45db28045395c10
divon85 <igornovid@outlook.com>
2025-06-19 12:01:22 +0900

    fix fonts

--- confs/system_modules/apps/software.nix
+++ confs/system_modules/apps/software.nix
@@ -38,6 +38,19 @@
         # Apps
         firefox                         # Web browser
         grimblast                       # Screenshot tools
+        
+        # Fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-emoji
+        font-awesome
+        nerd-fonts.symbols-only
+        fira-sans
+        fira-code
+        fira-code-symbols
+        roboto
+        liberation_ttf
+        texlivePackages.japanese-otf
     ];
 
     programs.firefox.enable = true;
