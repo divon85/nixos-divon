1ecd6c33ffeebadfb9bae5a8ac77b1b318c485df
divon85 <igornovid@outlook.com>
2025-06-19 17:01:10 +0900

    fix vscode

--- confs/home_modules/apps/vscode/usersettings.nix
+++ confs/home_modules/apps/vscode/usersettings.nix
@@ -2,7 +2,26 @@
     programs.vscode = {
         profiles.default.userSettings = {
             "files.autoSave" = "off";
-            "[nix]"."editor.tabSize" = 4;
+            "editor.tabSize" = 4;
+            "chat.editor.fontFamily" = "DejaVu Sans Mono";
+            "chat.editor.fontSize" = 20.0;
+            "debug.console.fontFamily" = "DejaVu Sans Mono";
+            "debug.console.fontSize" = 20.0;
+            "editor.fontFamily" = "DejaVu Sans Mono";
+            "editor.fontSize" = 20.0;
+            "editor.inlayHints.fontFamily" = "DejaVu Sans Mono";
+            "editor.inlineSuggest.fontFamily" = "DejaVu Sans Mono";
+            "editor.minimap.sectionHeaderFontSize" = 12.857142857142858;
+            "markdown.preview.fontFamily" = "DejaVu Sans";
+            "markdown.preview.fontSize" = 20.0;
+            "scm.inputFontFamily" = "DejaVu Sans Mono";
+            "scm.inputFontSize" = 18.571428571428573;
+            "screencastMode.fontSize" = 80.0;
+            "terminal.integrated.fontSize" = 20.0;
+            "update.mode" = "none";
+            "workbench.colorTheme" = "Stylix";
+            "workbench.iconTheme" = "vscode-icons";
+            "workbench.startupEditor" = "none";
         };
     };
 }
