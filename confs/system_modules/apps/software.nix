96a0c996eaad268dee1286a9f1d9b57435f2ecff
divon85 <igornovid@outlook.com>
2025-06-25 13:18:30 +0900

    remove unnecessary software

--- confs/system_modules/apps/software.nix
+++ confs/system_modules/apps/software.nix
@@ -43,7 +43,6 @@
         # Apps
         firefox                         # Web browser
         grimblast                       # Screenshot tools
-        hakuneko                        # Manga downloader
 
 
 
